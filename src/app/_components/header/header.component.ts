392bdfe4ee48a47bf14282bf99b9d581230b6372
Ivan Donyk <ivandonyk@gmail.com>
2019-05-01 17:09:53 +0300

    added logo

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -7,7 +7,7 @@ import {AddEquipmentComponent} from './add-equipment/add-equipment.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
