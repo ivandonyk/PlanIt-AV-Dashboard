cf6f4dde4a2da2bfffb187d067545324b0b22de8
Ivan Donyk <ivandonyk@gmail.com>
2019-05-01 13:19:02 +0300

    changed view of menu options, added active menu option

--- src/app/_components/sidebar/sidebar.component.ts
+++ src/app/_components/sidebar/sidebar.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarRoutesModule} from './sidebar-routes.module';
 import { GlobalVarsHelper } from '../../_helpers/global-vars';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  public currentRoute: string;
+
   constructor(
-    public globalVars: GlobalVarsHelper
-  ) { }
+    public globalVars: GlobalVarsHelper,
+    public router: Router
+  ) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.currentRoute = val.url;
+      }
+    });
+
+  }
 
   ngOnInit() {
     console.log(this);
