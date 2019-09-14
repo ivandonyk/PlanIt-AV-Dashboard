d57528706f306ec81e9124bda4833400f764723f
Ivan Donyk <ivandonyk@gmail.com>
2019-09-14 23:44:56 +0200

    - Defect - Clone Room validation - Add/Update Room Validation - Defect - Validation - Add Equipment

--- src/app/directive/maxlength.directive.ts
+++ src/app/directive/maxlength.directive.ts
@@ -12,9 +12,9 @@ export class MaxlengthDirective implements OnInit {
   }
 
   @HostListener('document:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    const maxLength = this.elementRef.nativeElement.maxLength;
-    const currentLength = this.elementRef.nativeElement.value.length;
+  handleKeyboardEvent(event: any) {
+    const maxLength = event.target.maxLength;
+    const currentLength = event.target.value.length;
     if (currentLength >= maxLength && (/Key/.test(event.code) || /Digit/.test(event.code))) {
       event.preventDefault();
     }
